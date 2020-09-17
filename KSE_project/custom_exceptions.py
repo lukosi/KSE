52453c29d4be978b16b8c3df0d0e0f87074e6138
Lukasz Osieleniec <losielen@gmail.com>
2020-09-17 21:18:06 +0200

    Add first custom exceptions to custom_exceptions.py

---
+++ KSE_project/custom_exceptions.py
@@ -0,0 +1,19 @@
+class PowerNotInRange(Exception):
+
+    def __init__(self, message="Error was raised while initializing class because condition 0 < power_min < power_max < 2500 was not met"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class YearNotInRange(Exception):
+
+    def __init__(self, message="Error was raised while initializing class because condition 1950 < year_start < year__end < 2100 was not met"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class PlantTypeNotInTypes(Exception):
+
+    def __init__(self, message="Error was raised while initializing class because plant type was not defined type for plant in program"):
+        self.message = message
+        super().__init__(self.message)
