167d28d98abfc1522dcbb0d2c3d0b1cc78073a88
Lukasz Osieleniec <losielen@gmail.com>
2020-09-30 20:29:59 +0200

    Add efficiency error handling class to custom_exceptions.py

--- KSE_project/custom_exceptions.py
+++ KSE_project/custom_exceptions.py
@@ -12,6 +12,13 @@ class YearNotInRange(Exception):
         super().__init__(self.message)
 
 
+class EfficiencyNotInRange(Exception):
+
+    def __init__(self, message="Error was raised while initializing class because condition 0 < efficiency < 1 was not met"):
+        self.message = message
+        super().__init__(self.message)
+
+
 class PlantTypeNotInTypes(Exception):
 
     def __init__(self, message="Error was raised while initializing class because plant type was not defined type for plant in program"):
